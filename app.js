db6a48d39d067c1f15252ccf42a426484759869f
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-11-08 15:21:04 -0500

    Created error template

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something went wrong' } = err;
-    res.status(statusCode).send(message);
-    res.send('Oh Boy, something went wrong!');
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
