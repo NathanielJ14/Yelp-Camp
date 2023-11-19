bf1e32e0e799caa5666eff18a158f684ec5f09d6
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-11-19 09:11:24 -0500

    Created public directory

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Setting up routes
