4bca5c93aed52c873180db37bb583e843ae128c0
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-11-30 15:11:39 -0500

    Create registration

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
@@ -60,8 +61,9 @@ app.use((req, res, next) => {
 })
 
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes);
 
 
 // Setting up routes
