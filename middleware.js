5e6ee2a964579ace44da7fb2c70951c7fee0c250
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-12-05 14:03:23 -0500

    Created review middleware

--- middleware.js
+++ middleware.js
@@ -52,3 +52,14 @@ module.exports.validateReview = (req, res, next) => {
         next();
     }
 }
+
+//Review author authorization middleware
+module.exports.isReviewAuthor = async(req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
