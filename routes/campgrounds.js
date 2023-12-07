9c360973e2fb1afbb9444d22a4621200b23fca35
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-12-07 17:09:22 -0500

    Set up cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/'});
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 
 //All campground routes
