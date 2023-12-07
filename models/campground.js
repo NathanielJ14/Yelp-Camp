5a92d09e4152de6566ad3c4dfece454e37f73e29
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-12-07 17:22:05 -0500

    Set up image uploader with cloudinary

--- models/campground.js
+++ models/campground.js
@@ -5,7 +5,12 @@ const Review = require('./review');
 // Setting up campground db
 const CampgroundSchema = new Schema({
     title: String,
-    image: String,
+    images: [
+        {
+            url: String,
+            filename: String
+        }
+    ],
     price: Number,
     description: String,
     location: String,
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,11 +11,7 @@ const upload = multer({ storage });
 //All campground routes
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-    .post(upload.single('image'), (req, res) => {
-        console.log(req.body, req.files);
-        res.send('It worked');
-    })
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
