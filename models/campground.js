fa22dafee2c2a75a9eda4bbde2d39b75c1113c17
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-12-16 12:43:26 -0500

    Adding thumbnail virtual property

--- models/campground.js
+++ models/campground.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+
+//Setting up image schema
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 // Setting up campground db
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
@@ -34,6 +40,6 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
             }
         })
     } 
-})
+});
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
