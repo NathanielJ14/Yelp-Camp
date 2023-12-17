1ca10e536029e7f5615fbb327caf9ce225e7f060
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-12-17 18:36:54 -0500

    Set up for deployment

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
+//mongodb://localhost:27017/yelp-camp
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
